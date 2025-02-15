068c6aae1520535e21908e8db9c07dd0e048d42d
SAFVAN CMC <safvan.cmc@tekgile.com>
2025-02-15 19:48:12 +0530

    words updated

--- src/util/getRandomWord.ts
+++ src/util/getRandomWord.ts
@@ -1,4 +1,4 @@
 // Define word list
-import words from './words.json'
+import {words} from './words'
 export const getRandomWord = (): string =>
   words[Math.floor(Math.random() * words.length)].word;
